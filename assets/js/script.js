cdf448d42fe19b89de78b4caab5dcf1bc14fad07
Wamackie <wamackie8456@gmail.com>
2021-08-05 00:36:55 -0400

    console log containerEl

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var date = moment().format('dddd MMMM Do');
+console.log(date)
+
+var time = moment().format('h A');
+console.log(time)
+
+var currentDayEl = document.getElementById('currentDay')
+
+var timeBlockElement = document.querySelector('.hour')
+
+console.log(timeBlockElement.textContent)
+
+
+
+for (i = 0; i < 9; i++) {
+    var containerEl = document.querySelector('.container')
+    console.log(containerEl.childNodes[i].childNodes[1].innerHtml)
+}
+
+currentDayEl.textContent = date
+
+//.childNodes[0]
