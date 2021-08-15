46ab8d830fda77f821d340e266cc626935442087
Wamackie <wamackie8456@gmail.com>
2021-08-15 13:10:43 -0400

    added screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,17 @@
 var date = moment().format('dddd MMMM Do');
-console.log('date', date)
 
 var time = moment().format('HH');
-console.log('time', time)
 
 var currentDayEl = document.getElementById('currentDay')
-
-var allTimeBlockEl = document.querySelectorAll('.hour')
 
 var descriptionEl = document.querySelectorAll('.description')
 
 var dataTimeTypeEl = document.getElementsByClassName('hour')
 
-var buttons = document.querySelectorAll('.saveBtn')
-
-console.log('dataTimeTypeEl', dataTimeTypeEl)
-
+// check time and color blocks based on past, present, and future
 checkTime();
 function checkTime() {
     for (i = 0; i < 9; i++) {
-        //console.log('allTimeBlockEl[i].textContent', allTimeBlockEl[i].textContent);
-        console.log('descriptionEl[i]', descriptionEl[i]);
-        console.log('dataTimeTypeEl[i]', dataTimeTypeEl[i]);
         dataTimeTypeEl[i].getAttribute("data-time");
         if (dataTimeTypeEl[i].dataset.time > parseInt(time)) {
             descriptionEl[i].classList.add('future');
@@ -33,14 +23,27 @@ function checkTime() {
     }
 }
 
-//save to local storage
-function logData() {
-    for (i=0; i < 9; i++){
-    console.log('button is working')
-    console.log(descriptionEl[i])
-    }
-}
+$(document).ready(function () {
+    // listen for save button clicks
+    $('.saveBtn').on('click', function () {
+      // get nearby values
+      var value = $(this).siblings('.description').val();
+      var parent = $(this).parent().attr('id');
+  
+      // save in localStorage
+      localStorage.setItem(parent, value);
+    });
+    
+      // load saved data from localStorage
+      $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+      $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+      $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+      $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+      $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+      $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+      $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+      $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+      $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+    });
 
 currentDayEl.textContent = date
-console.log(buttons)
-console.log(descriptionEl)
